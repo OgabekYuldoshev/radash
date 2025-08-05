31c1b034d661540901e182deaa6e588f9f71c23d
OgabekYuldoshev <yuldashoff1@gmail.com>
2025-08-05 21:45:14 +0500

    fix: improve object cloning and enhance get function to handle null values

--- src/object.ts
+++ src/object.ts
@@ -136,13 +136,13 @@ export const clone = <T>(obj: T): T => {
 
   // Access the constructor and create a new object.
   // This method can create an array as well.
-  const newObj = new ((obj as object).constructor as { new (): T })()
+  const newObj = new ((obj as object).constructor as { new(): T })()
 
   // Assign the props.
   Object.getOwnPropertyNames(obj).forEach(prop => {
     // Bypass type checking since the primitive cases
     // are already checked in the beginning
-    ;(newObj as any)[prop] = (obj as any)[prop]
+    ; (newObj as any)[prop] = (obj as any)[prop]
   })
 
   return newObj
@@ -225,6 +225,7 @@ export const get = <TDefault = unknown>(
     current = current[dequoted]
   }
   if (current === undefined) return defaultValue as TDefault
+  if (current === null) return defaultValue as TDefault
   return current
 }
 
